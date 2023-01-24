9280303687b6590eb40a7b4dd92e236c0c85ff92
Abhishek Gaire <abhisekgaire7@gmail.com>
2023-01-24 11:27:50 +0545

    CHalpal

---
+++ Practice/php.php
@@ -0,0 +1,28 @@
+<?php
+// display
+echo"This is string";
+
+// variable
+$name="Abhishek";
+?> dwofiuhaos fhasohfaskdfjhaslkjbg dghsgsdf
+dfg
+sdfg 
+dfsg
+sdfg sem_getdsg
+spl_autoload_functionsgs
+dfg <s>g sfsdfzb
+    sdfbg
+    zdsfgh
+    zsdftgh
+    <tfoot>gFAH
+        Bfdth
+        aet assertAErgh
+        afsgh
+         ER
+         ASFRGH
+         FSG
+          AEFSHG
+          AETHaef
+          
+    </tfoot>
+</s>
