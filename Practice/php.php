25dcc04e57993b2948883c97f9ece617ef4a4450
Abhishek Gaire <abhisekgaire7@gmail.com>
2023-01-27 07:34:32 +0545

    main

--- Practice/php.php
+++ Practice/php.php
@@ -4,25 +4,4 @@ echo"This is string";
 
 // variable
 $name="Abhishek";
-?> dwofiuhaos fhasohfaskdfjhaslkjbg dghsgsdf
-dfg
-sdfg 
-dfsg
-sdfg sem_getdsg
-spl_autoload_functionsgs
-dfg <s>g sfsdfzb
-    sdfbg
-    zdsfgh
-    zsdftgh
-    <tfoot>gFAH
-        Bfdth
-        aet assertAErgh
-        afsgh
-         ER
-         ASFRGH
-         FSG
-          AEFSHG
-          AETHaef
-          
-    </tfoot>
-</s>
+?> 
